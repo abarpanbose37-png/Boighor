b507977f6d2fd09e0c6205df59a302ad6a8023bc
abarpanbose37-png <abarpanbose37@gmail.com>
2026-08-24 23:52:08 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* আমার বইঘর — service worker */
-const CACHE = 'boighor-v3';
+const CACHE = 'boighor-v4';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
